13335d1d65bd07632207254662ccf8d3c6c2666c
WuPaulo <paulowbusiness@gmail.com>
2021-04-09 16:48:23 -0400

    added footer layout

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <footer>
+            <div className="footer-wrapper">
+                <p>Created @<a href="">Juno</a> Copyright 2021 by <a href="https://github.com/christiesunnie">Christie Tran</a>&<a href="https://github.com/WuPaulo">Paulo Wu</a></p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
