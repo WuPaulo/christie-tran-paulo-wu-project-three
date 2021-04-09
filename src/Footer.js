554644abd64973219a1aa2ec8389f895eff6238d
WuPaulo <paulowbusiness@gmail.com>
2021-04-09 17:06:16 -0400

    added footer :

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,8 @@ const Footer = () => {
     return (
         <footer>
             <div className="footer-wrapper">
-                <p>Created @<a href="">Juno</a> Copyright 2021 by <a href="https://github.com/christiesunnie">Christie Tran</a>&<a href="https://github.com/WuPaulo">Paulo Wu</a></p>
+                <p>Created &copy; <a href="">Juno</a></p>
+                 <p>Copyright 2021 by <a href="https://github.com/christiesunnie">Christie Tran</a> & <a href="https://github.com/WuPaulo">Paulo Wu</a></p>
             </div>
         </footer>
     )
