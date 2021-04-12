9c2d17eed5321bbc9c68873f47a1c3762191f0e1
WuPaulo <paulowbusiness@gmail.com>
2021-04-11 23:30:28 -0400

    added media queries

--- src/RestArea.js
+++ src/RestArea.js
@@ -3,49 +3,54 @@ import { regions, areaTypes } from "./selectArrays";
 import { useState } from "react";
 
 const RestArea = ({ handleSubmit }) => {
-    // Create useState for form select
-    const [regionSelect, setRegionSelect] = useState("Select Region");
-    const [areaTypeSelect, setAreaTypeSelect] = useState("Select Area Type");
+  // Create useState for form select
+  const [regionSelect, setRegionSelect] = useState("Select Region");
+  const [areaTypeSelect, setAreaTypeSelect] = useState("Select Area Type");
 
-    return (
-        <>
-            <form
-                onSubmit={(e) => handleSubmit(e, regionSelect, areaTypeSelect)}
-            >
-                {/* Select dropdown menus */}
-                <div className="select-container">
-                    {/* Region select dropdown */}
-                    <div className="select-inner-container">
-                        <Select
-                            id="Regions"
-                            values={regions}
-                            selectedValue="Select Region"
-                            onValueChange={(val) => {
-                                setRegionSelect(val);
-                            }}
-                        />
-                    </div>
+  return (
+    <>
+      <form onSubmit={(e) => handleSubmit(e, regionSelect, areaTypeSelect)}>
+        {/* Select dropdown menus */}
+        <div className="select-outter-container">
+          <div className="select-container">
+            {/* Region select dropdown */}
+            <div className="region-container">
+              <Select
+                id="Regions"
+                values={regions}
+                selectedValue="Select Region"
+                onValueChange={(val) => {
+                  setRegionSelect(val);
+                }}
+              />
+            </div>
 
-                    {/* Area type select dropdown */}
-                    <div className="select-inner-container">
-                        <Select
-                            id="Area Types"
-                            values={areaTypes}
-                            selectedValue="Select Area Type"
-                            onValueChange={(val) => {
-                                setAreaTypeSelect(val);
-                            }}
-                        />
-                    </div>
-                </div>
-
-                {/* Search button start */}
-                <button type="submit" className="btn-search">
-                    Search
-                </button>
-            </form>
-        </>
-    );
+            {/* Area type select dropdown */}
+            <div className="area-container">
+              <Select
+                id="Area Types"
+                values={areaTypes}
+                selectedValue="Select Area Type"
+                onValueChange={(val) => {
+                  setAreaTypeSelect(val);
+                }}
+              />
+            </div>
+            <div className="submit-container">
+              {/* Search button start */}
+              <button type="submit" className="btn-search">
+                Search
+              </button>
+            </div>
+          </div>
+          <p>
+            Select the region as well as the area type of your choice from the
+            dropdown menus, to search and generate data that matches!
+          </p>
+        </div>
+      </form>
+    </>
+  );
 };
 
 export default RestArea;
