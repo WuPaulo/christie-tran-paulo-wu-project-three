ebe08cb3011f5a9f5aa30199d897b5fd7ebc4df9
christiesunnie <christiesunnie@gmail.com>
2021-04-14 13:56:04 -0400

    Added feature for scroll up button

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import RoadCamera from "./components/RoadCamera";
 import DisplayRestAreaResults from "./components/DisplayRestAreaResults.js";
 import DisplayRoadCameraResults from "./components/DisplayRoadCameraResults.js";
 import Footer from "./components/Footer.js";
+import ScrollUpButton from "react-scroll-up-button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowCircleUp } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-scroll";
 
 function App() {
     // Create useState for result after the user click the search button
@@ -160,20 +160,20 @@ function App() {
                     )}
                 </div>
                 {/* Button to scroll up to the top page */}
-                <button
-                    className="btn-scroll-up"
-                    aria-label="Button scrolls to the top of the page"
+                <ScrollUpButton
+                    ContainerClassName="btn-scroll-up"
+                    TransitionClassName="btn-scroll-up-transition"
+                    StopPosition={0}
+                    ShowAtPosition={800}
+                    EasingType="easeInBounce"
                 >
-                    {/* Link to target the click scroll to the top page */}
-                    <Link to="home-page" spy={true} smooth={true}>
-                        <FontAwesomeIcon
-                            icon={faArrowCircleUp}
-                            size="3x"
-                            color="#f39b28"
-                            aria-hidden="false"
-                        />
-                    </Link>
-                </button>
+                    <FontAwesomeIcon
+                        icon={faArrowCircleUp}
+                        size="3x"
+                        aria-hidden="false"
+                    />
+                </ScrollUpButton>
+                {/* Link to target the click scroll to the top page */}
             </main>
             {/* Footer component */}
             <Footer />
